16ae3fa04c55f1b6b3384dda7ba3cd5dfeaed738
amitkatiyar <amit.katiyar@humanitics.com>
2026-09-02 00:58:22 +0530

    hide hindi button

--- app/src/main/java/com/amitbharat/songsplayer/ui/main/MainActivity.java
+++ app/src/main/java/com/amitbharat/songsplayer/ui/main/MainActivity.java
@@ -75,8 +75,14 @@ public class MainActivity extends AppCompatActivity {
             });
 
     @Override
+    protected void attachBaseContext(android.content.Context newBase) {
+        super.attachBaseContext(com.amitbharat.songsplayer.utils.LocaleHelper.wrapContext(newBase));
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        com.amitbharat.songsplayer.utils.LocaleHelper.applyAppLanguage(this);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
@@ -108,13 +114,6 @@ public class MainActivity extends AppCompatActivity {
     private long lastBackPressedTime = 0;
 
     private void setupToolbar() {
-        boolean isHindi = com.amitbharat.songsplayer.utils.LocaleHelper.isHindi(this);
-        binding.btnLanguageToggle.setText(isHindi ? "English" : "हिंदी");
-        binding.btnLanguageToggle.setOnClickListener(v -> {
-            String targetLang = com.amitbharat.songsplayer.utils.LocaleHelper.isHindi(this) ? "en" : "hi";
-            com.amitbharat.songsplayer.utils.LocaleHelper.setLocale(this, targetLang);
-        });
-
         binding.topAppBar.setOnMenuItemClickListener(item -> {
             int id = item.getItemId();
             if (id == R.id.action_search) {
